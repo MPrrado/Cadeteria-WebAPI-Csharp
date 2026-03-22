a4b350d343f11376cc6ae5ba838d4a025dc5659a
MPrrado <matias14prado@gmail.com>
2026-03-22 19:29:43 -0300

    implementacion para probar los endpoint con Scalar

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,12 +8,14 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference(); //agregamos para que se pueda usar scalar y poder probar los endpoints
 }
 
 app.UseHttpsRedirection();
