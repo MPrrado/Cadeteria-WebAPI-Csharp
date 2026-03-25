914dbeeed3e21a1ea273c34641817aa26ca66ac4
MPrrado <matias14prado@gmail.com>
2026-03-25 13:04:15 -0300

    Cambio en la inicializacion de mi cadeteria para poder tener persistencia con datos de prueba que se cargan desde archivos csv. Implementacion de end point: ObtenerListaPedidos, ObntenerListaPedidos y ObtenerInforme

--- Controllers/CadeteriaController.cs
+++ Controllers/CadeteriaController.cs
@@ -12,29 +12,36 @@ namespace EspacioCadeteriaController
     [Route("[controller]")]
     public class CadeteriaController : ControllerBase
     {
-        // [HttpGet]
-        // public IActionResult GetPedidos()
-        // {
-            
-        // }
-        private readonly string rutaArchivoCadeteria = "src/cadeteria.csv";
-        private readonly string rutaArchivoClientes = "src/clientes.csv";
-        private Cadeteria miCadeteria;  
-        public CadeteriaController()
+       
+        private static Cadeteria miCadeteria = InicializarCadeteria(); // aqui se ejecuta una vez debido a que es static, y se mantiene en memoria durante toda la vida de la aplicación.
+
+        
+        private static Cadeteria InicializarCadeteria()
         {
-            miCadeteria = AccesoADatos.CargarDatosCadeteria(rutaArchivoCadeteria);
-            List<Cadete>listadoCadetes =AccesoADatos.CargarListadoCadetes("src/cadetes.csv");
-            foreach (Cadete cadete in listadoCadetes)
+            Cadeteria nuevaCadeteria = AccesoADatos.CargarDatosCadeteria("src/cadeteria.csv");
+            List<Cadete> cadetesCSV = AccesoADatos.CargarListadoCadetes("src/cadetes.csv");
+
+        
+            foreach (Cadete cadete in cadetesCSV)
             {
-                miCadeteria.AgregarCadete(cadete.Nombre, cadete.Direccion, cadete.Telefono);
+                nuevaCadeteria.AgregarCadete(cadete.Nombre, cadete.Direccion, cadete.Telefono);
             }
+            
+            List<Pedido> pedidosCSV = AccesoADatos.CargarPedidos("src/pedidos.csv", nuevaCadeteria.ObtenerListadoCadetes());
+
+            foreach (Pedido pedido in pedidosCSV)
+            {
+                nuevaCadeteria.AltaPedido(pedido);
+            }
+
+            return nuevaCadeteria;
         }
 
-        [HttpPost]
+        [HttpPost("AltaPedido")]
         public IActionResult AltaPedido([FromBody]Pedido pedido)
         {
             if(pedido == null) return BadRequest("El pedido no puede ser nulo.");
-            if (miCadeteria.AltaPedido(pedido, rutaArchivoClientes))
+            if (miCadeteria.AltaPedido(pedido))
             {
                 return Created("Pedido agregado exitosamente.", pedido);
             }
@@ -43,5 +50,30 @@ namespace EspacioCadeteriaController
                 return StatusCode(500, "No se pudo agregar el pedido.");
             }
         }
+
+        [HttpGet("ObtenerListaPedidos")]
+        public IActionResult GetPedidos()
+        {
+            List<Pedido> pedidos = miCadeteria.ObtenerListadoPedido();
+            if(pedidos == null || pedidos.Count == 0) return NoContent();
+            return Ok(pedidos);
+        }
+
+        [HttpGet("ObtenerListadoCadetes")]
+        public IActionResult GetCadetes()
+        {
+            List<Cadete>Cadetes = miCadeteria.ObtenerListadoCadetes();
+            if(Cadetes == null || Cadetes.Count == 0) return NoContent();
+            return Ok(Cadetes);
+        }
+
+        [HttpGet("ObtenerInforme")]
+        public IActionResult GetInforme()
+        {
+            Informe informe = miCadeteria.GenerarInforme();
+            if(informe == null) return NoContent();
+            return Ok(informe);
+        }
+
     }
 }
